19feaeb1a925c746817c65a8b4503f25f0369c1d
AnthonyChen7 <hp_a1700@hotmail.com>
2019-03-03 13:47:03 -0800

    Added header

--- src/components/match-list/match-list.component.tsx
+++ src/components/match-list/match-list.component.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
 import './match-list.component.scss';
-import { Grid } from 'semantic-ui-react'
-import { SummonerMatchStatistic } from 'src/models/summoner-match-statistic';
+import { Grid, Header } from 'semantic-ui-react'
 import { SummonerMatches } from 'src/models/summoner-matches';
 
 interface MatchListProps {
-  // matches: SummonerMatchStatistic[]
   matches: SummonerMatches | undefined;
 }
 
@@ -14,16 +12,13 @@ export class MatchListComponent extends React.Component<MatchListProps, {}> {
     super(props);
   }
 
-  
-  componentDidUpdate(prevProps: MatchListProps) {
-    console.log(prevProps);
-  } 
   // DO NOT push boolean in JSX expression. It will fix this issue.
   // https://github.com/react-toolbox/react-toolbox/issues/1523
   render() {
     if (this.props.matches && this.props.matches.matchStats && this.props.matches.matchStats.length > 0) {
       return ( 
-        // <>
+        <>
+          <Header as='h1'>Match History for {this.props.matches.summonerName}</Header>
           <Grid padded>
             {this.props.matches.matchStats.map( (matchStat) => (
               <Grid.Row color={'red'} key={matchStat.id}>
@@ -31,7 +26,7 @@ export class MatchListComponent extends React.Component<MatchListProps, {}> {
               </Grid.Row>
             ))}
           </Grid>
-        // </>
+        </>
       );
     }
     else {
@@ -39,6 +34,5 @@ export class MatchListComponent extends React.Component<MatchListProps, {}> {
         <div>No match histories found</div>
       );
     }
-
   }
 }
